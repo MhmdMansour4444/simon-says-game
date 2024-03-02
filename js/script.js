4ea730a7fffae3a337a151da305b2c67f06b092b
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-02 19:29:18 +0200

    feat(randomSequence): random colorsequence now generates

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@ const infoText = document.getElementById("info");
 
 playButton.addEventListener("click", function () {
   infoText.textContent = "Lets go! Try to keep up with the sequence.";
+  function generateRandomSequence(length) {
+    const colors = ["green", "red", "blue", "yellow"];
+    const Colorsequence = [];
 
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      Colorsequence.push(colors[randomIndex]);
+    }
+    return Colorsequence;
+  }
+  const randomSequence = generateRandomSequence(12);
+  console.log(randomSequence);
 });
