7208c66a9191e3f8110193346e626595e7e85a61
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-02 19:25:20 +0200

    feat(play button): now functional

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const playButton = document.getElementById("play");
+const infoText = document.getElementById("info");
 
+playButton.addEventListener("click", function () {
+  infoText.textContent = "Lets go! Try to keep up with the sequence.";
+
+});
