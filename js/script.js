5956eae5287f649a926b069d783dc2534e97e251
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-03 02:35:14 +0200

    chore(fixes): added the onload and fixed the js link in html we cant touch

--- js/script.js
+++ js/script.js
@@ -1,130 +1,121 @@
-const playButton = document.getElementById("play");
-const infoText = document.getElementById("info");
-const maxSequenceLength = 4;
-const board = document.querySelector(".board");
-const levelElt = document.getElementById("level");
-const highScoreElt = document.getElementById("high-score");
-const audioFiles = {
-  blueSound: "sounds/blue.mp3",
-  redSound: "sounds/red.mp3",
-  yellowSound: "sounds/yellow.mp3",
-  greenSound: "sounds/green.mp3",
-  loseSound: "sounds/game-over.wav",
-  winSound: "sounds/game-win.wav",
-  wrongSound: "sounds/wrong.mp3",
-};
-let gameStarted = false;
-let playerSequence = [];
-let currentStep = 0;
-let currentLevel = 0;
-let randomSequence = [];
-let sequenceIndex = 1;
+window.onload = function () {
+  const playButton = document.getElementById("play");
+  const infoText = document.getElementById("info");
+  const board = document.querySelector(".board");
+  const levelElt = document.getElementById("level");
+  const highScoreElt = document.getElementById("high-score");
+  const audioFiles = {
+    blueSound: "sounds/blue.mp3",
+    redSound: "sounds/red.mp3",
+    yellowSound: "sounds/yellow.mp3",
+    greenSound: "sounds/green.mp3",
+    loseSound: "sounds/game-over.wav",
+    winSound: "sounds/game-win.wav",
+    wrongSound: "sounds/wrong.mp3",
+  };
+  let sequence = [];
+  let tilesSelected = [];
+  const colors = ["green", "red", "blue", "yellow"];
+  let score = 0;
+  let highestScore = 0;
 
-let sequence = [];
-let tilesSelected = [];
-const colors = ["green", "red", "blue", "yellow"];
-let score = 0;
-let highestScore = 0;
-
-let tiles = document.querySelectorAll(".tile");
-tiles.forEach((el, index) => {
-  el.addEventListener("click", (e) => {
-    playerClicks(colors[index]);
+  let tiles = document.querySelectorAll(".tile");
+  tiles.forEach((el, index) => {
+    el.addEventListener("click", (e) => {
+      playerClicks(colors[index]);
+    });
   });
-});
 
-playButton.addEventListener("click", function () {
-  infoText.textContent = "Let's go! Try to keep up with the sequence until 12 to win!!";
-  resetScore();
-  resetSequence();
-  extendSequence();
-  displaySequence(sequence);
-});
+  playButton.addEventListener("click", function () {
+    infoText.textContent =
+      "Let's go! Try to keep up with the sequence until 12 to win!!";
+    resetScore();
+    resetSequence();
+    extendSequence();
+    displaySequence(sequence);
+  });
 
-function displaySequence(sequence) {
-  const board = document.querySelector(`.board`);
-  board.classList.add("unclickable");
-
-  sequence.forEach(function (color, index) {
-    setTimeout(function () {
-      const tile = document.querySelector(`[data-tile="${color}"]`);
-      tile.classList.remove("inactive");
-      const audio = new Audio(audioFiles[color]);
+  function displaySequence(sequence) {
+    board.classList.add("unclickable");
+    sequence.forEach(function (color, index) {
       setTimeout(function () {
-        tile.classList.add("inactive");
-        board.classList.remove("unclickable");
-      }, 500);
-    }, index * 700);
-  });
-}
-
-//// new functions
-
-function resetSelection() {
-  tilesSelected.splice(0, tilesSelected.length);
-}
-
-function resetSequence() {
-  sequence.splice(0, sequence.length);
-}
-
-function isSequenceComplete() {
-  return sequence.length === tilesSelected.length;
-}
-
-function extendSequence() {
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  sequence.push(colors[randomIndex]);
-  levelElt.innerHTML = sequence.length;
-}
-
-function isSequenceCorrect() {
-  for (let i = 0; i < tilesSelected.length; i++) {
-    const colorSelected = tilesSelected[i];
-    const seqColor = sequence[i];
-    if (colorSelected != seqColor) return false;
+        const tile = document.querySelector(`[data-tile="${color}"]`);
+        tile.classList.remove("inactive");
+        setTimeout(function () {
+          tile.classList.add("inactive");
+          board.classList.remove("unclickable");
+        }, 500);
+      }, index * 700);
+    });
   }
-  return true;
-}
 
-function increaseScore() {
-  score++;
-  if (score > highestScore) highestScore = score;
-  highScoreElt.innerHTML = highestScore;
-}
+  //// new functions
 
-function resetScore() {
-  score = 0;
-}
+  function resetSelection() {
+    tilesSelected.splice(0, tilesSelected.length);
+  }
 
-function GameEnded(winState) {
-  if (winState) alert("You Won :)");
-  else alert("You Lost!");
-  resetSelection();
-  resetSequence();
-  resetScore();
-}
+  function resetSequence() {
+    sequence.splice(0, sequence.length);
+  }
 
+  function isSequenceComplete() {
+    return sequence.length === tilesSelected.length;
+  }
 
-function playerClicks(selection) {
-  tilesSelected.push(selection);
-  playSound(selection);
+  function extendSequence() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    sequence.push(colors[randomIndex]);
+    levelElt.innerHTML = sequence.length;
+  }
 
-
-  console.log("PlayerSelected");
-  console.log(tilesSelected);
-
-  if (isSequenceCorrect()) {
-    // Sequence is Correct
-    if (isSequenceComplete()) {
-      extendSequence();
-      resetSelection();
-      increaseScore();
-      displaySequence(sequence);
+  function isSequenceCorrect() {
+    for (let i = 0; i < tilesSelected.length; i++) {
+      const colorSelected = tilesSelected[i];
+      const seqColor = sequence[i];
+      if (colorSelected != seqColor) return false;
     }
-  } else {
-    //Sequnce is Wrong
-    GameEnded(false);
-    return;
+    return true;
   }
-}
+
+  function increaseScore() {
+    score++;
+    if (score > highestScore) highestScore = score;
+    highScoreElt.innerHTML = highestScore;
+  }
+
+  function resetScore() {
+    score = 0;
+  }
+
+  function GameEnded(winState) {
+    if (winState) alert("You Won :)");
+    else alert("You Lost!");
+    resetSelection();
+    resetSequence();
+    resetScore();
+  }
+
+  function playerClicks(selection) {
+    tilesSelected.push(selection);
+    const audio = new Audio(audioFiles[selection]);
+    audio.play();
+    console.log("PlayerSelected");
+    console.log(tilesSelected);
+
+    if (isSequenceCorrect()) {
+      // Sequence is Correct
+      if (isSequenceComplete()) {
+        extendSequence();
+        resetSelection();
+        increaseScore();
+        displaySequence(sequence);
+      }
+    } else {
+      //Sequence is Wrong
+      GameEnded(false);
+
+      return;
+    }
+  }
+};
